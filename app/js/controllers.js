3242888b30adfebc205834ab3e120d13917be0e2
Kevin Lin <whatakevin@gmail.com>
2014-08-12 02:03:06 -0500

    Using angularfire

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,6 +6,11 @@ angular.module('myApp.controllers', [])
   .controller('BlogController', ['$scope', function($scope) {
 
   }])
-  .controller('PostController', ['$scope', function($scope) {
-
+  .controller('PostController', ['$scope', '$firebase', function($scope, $firebase) {
+    var ref = new Firebase("https://klinblog.firebaseio.com/");
+    // create an AngularFire reference to the data
+    var sync = $firebase(ref);
+    // download the data into a local object
+    var syncObject = sync.$asObject();
+    syncObject.$bindTo($scope, "data");
   }]);
