33ca164187619eddb3c41835e684247ddf6f3cea
Kevin Lin <whatakevin@gmail.com>
2014-08-18 04:17:46 -0500

    Implement interactive HTML reference

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -22,4 +22,46 @@ angular.module('myApp.controllers', [])
       $scope.newPostTitle = "";
       $scope.newPostBody = "";
     };
+
+    var addHTMLTag = function(tag){
+      if($scope.newPostBody){
+        $scope.newPostBody += tag;
+      }
+      else{
+        $scope.newPostBody = tag;
+      }
+    };
+    $scope.addParagraph = function(){
+      addHTMLTag('<p></p>');
+    };
+    $scope.addHorizontalRule= function(){
+      addHTMLTag('<hr>');
+    };
+    $scope.addLink = function(){
+      addHTMLTag('<a href="#"></a>');
+    };
+    $scope.addEmphasis = function(){
+      addHTMLTag('<em></em>');
+    };
+    $scope.addStrong= function(){
+      addHTMLTag('<strong></strong>');
+    };
+    $scope.addBlockquote = function(){
+      addHTMLTag('\n<blockquote>\n  <p></p>\n</blockquote>');
+    };
+    $scope.addHeading = function(){
+      addHTMLTag('<h2></h2>');
+    };
+    $scope.addSubheading = function(){
+      addHTMLTag('<h3></h3>');
+    };
+    $scope.addCodeBlock = function(){
+      addHTMLTag('<pre><code></code></pre>');
+    };
+    $scope.addUnorderedList = function(){
+      addHTMLTag('\n<ul>\n  <li></li>\n</ul>');
+    };
+    $scope.addOrderedList = function(){
+      addHTMLTag('\n<ol>\n  <li></li>\n</ol>');
+    };
   }]);
